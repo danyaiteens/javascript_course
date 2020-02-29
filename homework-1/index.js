c3c45b26ef5e8ed31d7cf7d40b131eeb9cd31604
danyaiteens <danyaiteens@gmail.com>
2020-02-29 21:28:09 +0200

    ddd

---
+++ homework-1/index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+let a;
+console.log(a);
+console.log(typeof a);
+
+a = 34;
+console.log(a);
+console.log(typeof a);
+
+let b = "30";
+b = 30;
+console.log(b);
+console.log(typeof b);
+
+let c = false;
+c = true;
+console.log(c);
+console.log(typeof c);
+
+var d = 20;
+console.log(d);
+console.log(typeof d);
+
+const name = "Alex";
+console.log(name);
+console.log(typeof name);
+//name = "KATE";
+//console.log(name);
+//console.log(typeof name);
+
+const isAutheneicad = true;
+console.log(isAutheneicad);
+console.log(typeof isAutheneicad);
+
+const isModalOpen = false;
+console.log(isModalOpen);
+console.log(typeof isModalOpen);
+
+const fe_group = null;
+console.log(fe_group);
+console.log(typeof fe_group);
+
+const massage = "Hello";
+console.log(massage);
+
+let man = "Ben";
+console.log(man);
+console.log(`${man}, ${massage}`);
+console.log("${man}, ${massage}");
+
+//alert(`${man}, ${massage}`);
+
+//const isReady = confirm("Are u Ready?");
+//alert(isReady);
+
+const enterName = prompt("Please give your mobile!");
+console.log(enterName);
